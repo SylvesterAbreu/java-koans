6a7df22e1180207b8b7199dac0253bd0bc584e1f
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-07-01 19:25:56 +0100

    Asserting an object is the same when comparing with it self

--- koans/src/beginner/AboutEquality.java
+++ koans/src/beginner/AboutEquality.java
@@ -25,8 +25,8 @@ public class AboutEquality {
 	@Koan
 	public void equalsMethodCanBeChangedBySubclassesToTestsIfTwoObjectsAreEqual(){
 		Object object = new Integer(1);
-		assertTrue(object.equals(object));
-		assertFalse(object.equals(new Integer(1)));
+		assertSame(object, object);
+		assertNotSame(object, (new Integer(1)));
 		// Note: This means that for the class 'Object' there is no difference between 'equal' and 'same'
 	}
 
