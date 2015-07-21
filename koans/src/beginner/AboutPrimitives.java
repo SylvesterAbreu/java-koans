ed85794327933f5a144fd9f2d1a5b5c31f4634e8
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-07-21 08:55:20 +0100

    Asserting that byte have a smaller range than short

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -90,7 +90,7 @@ public class AboutPrimitives {
 	@Koan
 	public void bytesHaveASmallerRangeThanShorts() {
 		assertEquals(Byte.MIN_VALUE, (byte)-128);
-		assertEquals(Byte.MAX_VALUE, __);
+		assertEquals(Byte.MAX_VALUE, (byte)127);
 
 		// Why would you use short or byte considering that you need to do explicit casts?
 	}
